9d09a260bbd59e650b6838d68a78f544b17add5d
odinde <odindelindi@gmail.com>
2023-04-20 18:43:38 -0700

    puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h
+/**
+ * main - a program that prints line with put functions
+ * Return:
+  */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
