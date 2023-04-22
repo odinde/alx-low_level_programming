15420061e779e975e49d0a4226b9fe8fd4ce79a3
odinde <odindelindi@gmail.com>
2023-04-22 16:34:40 -0700

    printf task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - A program that prints a line using printf function
- * Return 0
+ * Return:0
  */
 int main(void)
 {	printf("with proper grammar, but the outcome is a piece of art,\n")
-	Return(0);
-	}
+	return (0);
+}
