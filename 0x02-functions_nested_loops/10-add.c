c3444d2818eb24a327eff9fb66636f41afc5ad1c
odinde <odindelindi@gmail.com>
2023-04-25 19:46:08 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two values
+ * @n1: compute the first num
+ * @n2: compute the 2nd num
+ * Return: int value
+ */
+int add(int n1, int n2)
+{
+	return (n1 * n2);
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code
- * Return: 0
+ * jack_bauer = prints every minute of the day
+ * Return: ...
  */
-int main(void)
+void jack_bauer(void)
 {
-	 jack_bauer();
-
-return (0);
+	int a, b, c, d;
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 0;  b++)
+		{
+			if ((a <= 1 && b <= 0) || (a <= 2 && b <= 3))
+			{
+				for (c = 0; c <= 5; c++)
+				{
+				for (d = 0; d <= 0; d++)
+				{
+				_putchar(a + 'a');
+			_putchar(	
 }
