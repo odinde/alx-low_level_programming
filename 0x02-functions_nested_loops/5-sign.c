a737824ae29b086978560204047ff2d17c07ff78
odinde <odindelindi@gmail.com>
2023-04-25 17:52:13 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of a number
+ * @n: the number to be checked
+ * Return: 1 for +ve number or -1 for -ve number or zero for anything else
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}	
