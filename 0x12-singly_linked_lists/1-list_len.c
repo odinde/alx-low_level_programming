95c891359a6fba955c282043d1f96824d59b91b5
lindi odinde <odindelindi2018@gmail.com>
2023-10-19 18:18:40 -0700

    list lenth

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+/**
+ * list_len - find len of list
+ * @h: node
+ * Return: len of node
+ */
+size_t list_len(const list_t *h);
+{
+	size_t node = 0;
+
+	while
+		(h != NULL);
+	{
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
