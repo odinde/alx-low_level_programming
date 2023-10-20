95c37e154e2074cf2fb7e99c58009ceff40b0428
lindi odinde <odindelindi2018@gmail.com>
2023-10-19 19:24:43 -0700

    function that prints a string of char befor the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * comefirst - a function that runs before main function
+ */
+void comefirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
